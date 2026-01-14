a2439d1746ecb1c94c57d767d8cd4dd61a570234
Evyvicentesolis <evvi9638@colorado.edu>
2026-01-14 13:50:38 -0700

    Lecture 1/14/26

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+//input output stream
+//c++ library
+//more files = more frequent
 
+
+#include <iostream>
+// standard = std
 using namespace std;
 
-int main() {
+// function {}
+//int = integer
+// variable return type
+//main function = the name of thid funciton is main
+// main always returns an int
+// main never has parameters
+
+
+
+
+int main(/*parameters*/) {
+    // purpose of main = root of your code
+    //cout = console output
+    //\n = new line
+
     cout << "Hello World!\n";
+    // return line is optional
+    //return 0;
 }
