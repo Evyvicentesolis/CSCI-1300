447002456c84d12f66703ace50735dd5bf4b1dc2
Evyvicentesolis <evvi9638@colorado.edu>
2026-01-16 16:25:46 -0700

    Friday warmup

---
+++ FridayWarmup.cpp
@@ -0,0 +1,14 @@
+// input output stream
+// C++ library
+#include <iostream>                              // TODO: Include the correct library
+
+// standard namespace
+using namespace std;                        // TODO: Specify the correct namespace
+
+int main() {                                // TODO: What variable type is the return type?
+    // Console output
+    cout << "Hello World!\n";                // TODO: What is the output stream object?
+    cout << "Hello World!" << endl;       // TODO What is the alt way to say next line?
+
+    return 0;                             // TODO What is the optional return value for a successful main function?
+}
